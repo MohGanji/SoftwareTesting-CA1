58a3106c5b58fca28224ee73ae3e3b1524f1c539
Mohammad <mfg1376@gmail.com>
2018-10-13 14:26:59 +0330

    added script to run all tests, sample commands in readme

--- UserTest.java
+++ UserTest.java
@@ -4,10 +4,11 @@ import org.junit.Test;
 
 public class UserTest {
     @Test
-    public void evaluatesExpression() {
+    public void testConstructor() {
         User user = new User("foo", "bar");
         User user2 = new User("spa", "zzz");
         user.addFriend(user2);
         assertEquals(true, user.isFriendsWith(user2));
+        assertEquals(true, user2.isFriendsWith(user));
     }
 }
